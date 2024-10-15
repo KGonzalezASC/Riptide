2d1182e56624318b2d4f0989356c8c5acf89f022
Owen Gahagan <opg4403@g.rit.edu>
2024-10-15 19:17:12 -0400

    Implemented grind objects and relevant code in FishMovement
    There's still some jank when the player touches colliders, though--will work on this more.

---
+++ Assets/Scripts/GrindObjectKillTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrindObjectKillTrigger : MonoBehaviour
+{
+    private GrindableObject parentScript;
+
+    private void Awake()
+    {
+        parentScript = GetComponentInParent<GrindableObject>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void onTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            parentScript.killPlayer();
+            Debug.Log("Player hit kill box");
+        }
+    }
+}
