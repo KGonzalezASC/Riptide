8bf71c72d8041b108f9f63a3d63f5f88efe3dc9b
Nathan <nvm7252@rit.edu>
2024-10-26 16:15:38 -0400

    Added scores displaying to lose UI

--- Assets/Scripts/YouLoseState.cs
+++ Assets/Scripts/YouLoseState.cs
@@ -3,19 +3,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.UIElements;
 using Object = UnityEngine.Object;
 
 public class YouLoseState : gState
 {
 
-    [SerializeField] private MainMenuEvents uiGameObject;
-
+    [SerializeField] private UIDocument loseUIDocument; //lose screen ui document
+    private VisualElement scoreContainer;
+    private Label _label;
     private PlayState playState;
 
     public override void Enter(gState from)
     {
         //get a reference to previous game state as a play state
         playState = from as PlayState;
+
+        // Initialize score container
+        scoreContainer = loseUIDocument.rootVisualElement.Q<VisualElement>("container-score"); // Replace with actual container ID
+        PopulateScores();
+
+        // Add the player's new score to highscores
+        PlayerSaveData.Instance.InsertScore(playState.ScoreTracker.Score, "Player"); // TODO: add name input
     }
 
 
@@ -24,10 +33,29 @@ public class YouLoseState : gState
 
     }
 
+    /// <summary>
+    /// Populates the score container on lose UI
+    /// </summary>
+    private void PopulateScores()
+    {
+        //clear existing scores
+        scoreContainer.Clear();
+
+        foreach (HighscoreEntry entry in PlayerSaveData.Instance.highscores)
+        {
+            //create label
+            Label scoreLabel = new Label($"{entry.name}: {entry.score}");
+            //add styling
+            scoreLabel.AddToClassList("label-score");
+            //add to container
+            scoreContainer.Add(scoreLabel);
+        }
+    }
+
     public override void Exit(gState to)
     {
         //TODO: implement some way to type in name
-        PlayerSaveData.Instance.InsertScore(playState.ScoreTracker.Score, "");
+        PlayerSaveData.Instance.Save();
     }
 
     public override string GetName()
